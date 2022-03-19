fb23a03cd9de57b06bbb41d2a68b0346b458d759
Siddhant <santoshshrivastava39@gmail.com>
2022-03-19 17:25:06 +0530

    UC-1 Solved

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(56);
+            linkedList.Display();
+        }
+    }
+}
