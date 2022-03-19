5232435fb12369fefbaf0e7d656cca85a66ff966
Siddhant <santoshshrivastava39@gmail.com>
2022-03-19 22:54:06 +0530

    UC-3 Solved

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Operations");
+            LinkedList list = new LinkedList();
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            list.Display();
+        }
+    }
+}
