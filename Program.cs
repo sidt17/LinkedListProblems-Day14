08300e8fe413d3932be07cfa7e50335a1a6f9960
Siddhant <santoshshrivastava39@gmail.com>
2022-03-19 19:25:14 +0530

    UC-4 Solved

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(70);
+            Console.WriteLine("The Linkedlist before inserting 30 is: ");
+            list.Display();
+            list.InsertAtParticularPosition(2, 30);
+            Console.WriteLine("The Final Linkedlist after inserting 30 is: ");
+            list.Display();
+
+
+        }
+    }
+}
