9c666116edac20a2c6b77612b66299b592ff1999
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 00:00:04 +0530

    UC-5 Solved

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            list.RemoveFirstElement();
+            Console.WriteLine("The list after removing first element is : ");
+            list.Display();
+
+
+
+
+
+
+        }
+    }
+}
