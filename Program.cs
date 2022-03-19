c80749063faf6b82e5d07376aab569c64bde61b0
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 00:37:41 +0530

    UC-7 Solved

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List sequence is");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+
+            Console.WriteLine("-------------------------");
+            
+            Console.WriteLine("Search element");
+            list.Search(56);
+            list.Display();
+        }
+    }
+}
