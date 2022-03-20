45ad213df90e5aaead87bb748693d9a948b42847
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 15:39:45 +0530

    UC-9 Solved

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30); 
+            list.Add(40);
+
+            list.Add(70);
+            Console.WriteLine("The LinkedList Sequence is :");
+            list.Display();
+
+            Console.WriteLine("-------------------------");
+            
+            Console.WriteLine("After deleting 40: ");
+            list.Delete(40);
+            list.Display();
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("The size of the Linkedlist is :");
+            list.Size();
+          
+            
+        }
+    }
+}
