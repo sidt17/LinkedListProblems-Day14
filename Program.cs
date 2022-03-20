ff256026874f3843dd8dfca12568263f9a84f7dd
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 16:04:30 +0530

    UC-10 Solved

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedLinkedList list = new SortedLinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(40);
+            list.Add(70);
+
+            Console.WriteLine("The ordered list after  sorting : ");
+            list.Display();
+
+
+
+        }
+    }
+}
