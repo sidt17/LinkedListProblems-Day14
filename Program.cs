34d38733896af7ffe98085a83fe6bd986869e82f
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 16:46:46 +0530

    UC-2 Solved

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            LinkedList List = new LinkedList();
+            List.AddLast(70);
+            List.AddFirst(30);
+            List.AddFirst(56);
+            Console.WriteLine("Final LinkedList Sequence is :");
+            List.Display();
+        }
+    }
+}
