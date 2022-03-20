83c0d543a012695fdb858752d02c8628d1b564e3
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 15:10:11 +0530

    UC-8 Solved

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List sequence is");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Search element is ");
+            list.Search(30);
+            list.Display();
+            Console.WriteLine("-------------------------");
+            list.InsertAtParticularPosition(2, 40);
+            Console.WriteLine("The list after inserting 40 is :  ");
+            list.Display();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Final sequence of list is ");
+            list.InsertAtParticularPosition(1, 56);
+            list.Display();
+            
+        }
+    }
+}
