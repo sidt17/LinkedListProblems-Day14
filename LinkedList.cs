ffa7e5ebd8c81d1477b757eac092d2d76d51ab82
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 15:42:03 +0530

    Delete 40 and show the size

---
+++ LinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+           
+        }
+
+        
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+        // delete element
+        public void Delete(int data)
+        {
+            Node temp = head, previous = null;
+            if (temp != null && temp.data == data)
+            {
+                head = temp.next;
+                return;
+            }
+            while (temp != null && temp.data != data)
+            {
+                previous = temp;
+                temp = temp.next;
+            }
+            if (temp == null)
+                return;
+            previous.next = temp.next;
+        }
+        public int Size()
+        {
+            int size = 0;
+            Node temp = head;
+            while (temp != null)
+            {
+                size++;
+                temp = temp.next;
+
+            }
+            Console.WriteLine(size);
+            return size;
+        }
+
+    }
+}
+
