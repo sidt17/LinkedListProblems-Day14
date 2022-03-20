16ea90a579ca75c6018555377343db929a879f88
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 16:07:12 +0530

    Create order linkedlist

---
+++ SortedLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class SortedLinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            Node current = head, previous = null;
+            while (current != null && current.data < node.data)
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (previous == null)
+                head = node;
+            else
+            {
+                previous.next = node;
+            }
+            node.next = current;
+
+        }
+        public void Display()
+        {
+            if (this.head == null)
+                Console.WriteLine("The list is empty.");
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+
+    }
+}
+
